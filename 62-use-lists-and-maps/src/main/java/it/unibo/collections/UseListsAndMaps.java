bf3bb9b3a1b8d16fedc4e09b05008a4d6c1089a1
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-10-27 20:41:50 +0100

    first three points of exercise 62

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,5 +1,7 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,16 +23,25 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        final List<Integer> arrayList = new ArrayList<>(1000);
+        for (int i = 1000; i < 2000; i++) {
+            arrayList.add(i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        final List<Integer> linkedList = new LinkedList<>(arrayList);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        final int size = arrayList.size();
+        final int temporaryVar = arrayList.get(size-1);
+        arrayList.set(size-1, arrayList.get(0));
+        arrayList.set(0, temporaryVar);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
