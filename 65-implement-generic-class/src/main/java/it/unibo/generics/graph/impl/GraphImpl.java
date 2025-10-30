cfcb339adf07081a6dfa5b65c170486294707f2d
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-10-30 19:34:06 +0100

    Exercise 65: added the Graph interface methods to the GraphImpl class and left unimplemented

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,3 +1,40 @@
-package it.unibo.generics.graph.impl
+package it.unibo.generics.graph.impl;
 
-class
+import java.util.List;
+import java.util.Set;
+
+import it.unibo.generics.graph.api.Graph;
+
+class GraphImpl<N> implements Graph<N> {
+
+    @Override
+    public void addNode(N node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNode'");
+    }
+
+    @Override
+    public void addEdge(N source, N target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addEdge'");
+    }
+
+    @Override
+    public Set<N> nodeSet() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nodeSet'");
+    }
+
+    @Override
+    public Set<N> linkedNodes(N node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'linkedNodes'");
+    }
+
+    @Override
+    public List<N> getPath(N source, N target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPath'");
+    }
+
+}
